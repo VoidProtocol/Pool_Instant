4c9abb7c9b29a29404f5735ee1cf625e84154047
VoidProtocol <mw.orchowski@gmail.com>
2020-11-03 14:10:31 +0100

    PI-11: Minor tweaks, fixed bug when you can place white ball anywhere

--- Assets/Scripts/PlaceWhiteBall.cs
+++ Assets/Scripts/PlaceWhiteBall.cs
@@ -5,9 +5,17 @@ public class PlaceWhiteBall : MonoBehaviour
     [Header("Setup:")]
     [SerializeField] private GameObject _whiteBall;
 
+    private Collider _coll;
+
+    private void Awake()
+    {
+        _coll = GetComponent<Collider>();
+    }
+
     private void Update()
     {
-        if (TouchInput.GetIsScreenTouched)
+        if (TouchInput.GetIsScreenTouched && Mathf.Abs(TouchInput.GetTouchPosition.x) <= Mathf.Abs(_coll.bounds.size.x / 2.0f) && 
+            Mathf.Abs(TouchInput.GetTouchPosition.z) <= Mathf.Abs(_coll.bounds.size.z / 2.0f))
         {
             _whiteBall.transform.position = new Vector3(TouchInput.GetTouchPosition.x, 0.817f, TouchInput.GetTouchPosition.z);
             _whiteBall.SetActive(true);
